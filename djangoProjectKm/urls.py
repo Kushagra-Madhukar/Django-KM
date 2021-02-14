9c38baad376b0a1177cb76acfdd8af1ada4aefd9
Kushagra-Madhukar <madhukarkushagra@gmail.com>
2021-02-14 23:19:39 +0530

    Initial commit before push

--- djangoProjectKm/urls.py
+++ djangoProjectKm/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "KM Admin"
+admin.site.site_title = "KM Admin Portal"
+admin.site.index_title = "Welcome to KM Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
--- home/views.py
+++ home/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is Homepage")
+    context = {
+        "variable1": "This is a variable sent from views",
+        "variable2": "This is another variable sent from views"
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse("This is Homepage")
 
 def about(request):
-    return HttpResponse("This is About Page")
+    return render(request, 'about.html')
+    # return HttpResponse("This is About Page")
+
+def contact(request):
+    return render(request, 'contact.html')
+    # return HttpResponse("This is Contact Page")
+
+def services(request):
+    return render(request, 'services.html')
